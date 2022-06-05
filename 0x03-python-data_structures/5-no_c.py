de3f4bc33f3c29e768ab1affd6a6a19a0e680066
Abdisa Alemu <abdisa.alemu.wako@gmail.com>
2022-06-05 03:06:53 +0000

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    l = list(my_string)
-    for x in l:
-        if x == 'c':
-            l.remove('c')
-        if x == 'C':
-            l.remove('C')
-    my_string = "".join(l)
-    return my_string
+    """Remove all characters c and C from a string."""
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
