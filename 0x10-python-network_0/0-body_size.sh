c67ba01952e82819c514c575584dbc17782afb5d
Abdisa Alemu <abdisa.alemu.wako@gmail.com>
2022-09-22 18:23:33 +0000

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,3 @@
-ends a request an URL, and displays the size of the body of the response
+#!/bin/bash
+# sends a request an URL, and displays the size of the body of the response
 curl -s "$1" | wc -c
