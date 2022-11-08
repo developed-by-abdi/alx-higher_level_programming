db7efffdb21931586804bf36592f9ecdc006325a
Abdisa Alemu <abdisa.alemu.wako@gmail.com>
2022-11-08 14:20:35 +0300

    fixing

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(`code: ${response.statusCode}`);
-  }
+
+request(process.argv[2], function (_err, res) {
+  console.log('code:', res.statusCode); // Print the response status code if a response was received
 });
